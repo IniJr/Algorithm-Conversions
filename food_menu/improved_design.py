a46db67907f1adb15dd2e4fda89a468e31dbca5e
incified <iniobongdicksonjunior@gmail.com>
2022-08-16 22:51:17 +0100

    styling terminal

--- food_menu/improved_design.py
+++ food_menu/improved_design.py
@@ -1,10 +1,12 @@
 from rich.console import Console
 from rich.text import Text
 from rich.panel import Panel
+from rich import print
 
 
-console = Console()
+# console = Console()
 text = Text("Welcome to Mk’s Cuisine", justify="center")
 text.stylize("bold red")
 panel = Panel(text)
-console.print(panel)
+# console.print(panel)
+print(Panel(text, title="Welcome", subtitle="Thank you"))
